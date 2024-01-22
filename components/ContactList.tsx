009ba747db6422b7bb7bb9f0da48a5280f3e916c
SorathiaRutvik <sorathiarutvik2000>
2024-01-22 10:40:07 +0530

    feat: array functionality

---
+++ components/ContactList.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ContactList() {
+    const list = [
+        {
+            id: '1',
+            name: 'Rutvik',
+            phone: '1234567890',
+            email: 'rutvik@gmail.com,'
+        },
+        {
+            id: '2',
+            name: 'Raj',
+            phone: '7874408892',
+            email: 'raj@gmail.com',
+        },
+        {
+            id: '3',
+            name: 'Sagar',
+            phone: '7744229933',
+            email: 'sagar@gmail.com',
+        },
+    ]
+  return (
+    <View>
+          <Text>Contact List</Text>
+          {list.map((item) => (
+                <View key = {item.id}>
+                    <Text>{item.name}</Text>
+                    <Text>{item.phone}</Text>
+                    <Text>{item.email}</Text>
+                </View>
+          ))}
+          <View>
+              
+          </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
